407be1a3ac0476440c9debdb28a22731a8af256b
DavidGarciaFer <davidgar.fernandez@gmail.com>
2017-03-02 12:46:32 +0100

    Organizo correctamente el codigo en las carpetas de entrega

---
+++ Practica2/GR2201_LuisDavid/Apartado4/src/eps/uam/ads/p2/autoescuela/ejercicio4/Contrato.java
@@ -0,0 +1,113 @@
+package eps.uam.ads.p2.autoescuela.ejercicio4;
+
+/**
+ * Esta clase relaciona las clases Autoescuela y Profesor, para
+ * conseguir que cierta sede pueda tener un numero determinado de
+ * profesores contratados en un periodo de tiempo.
+ * 
+ * @author David Garcia (david.garcia03@estudiante.uam.es)
+ * @author Luis Carabe (luis.carabe@estudiante.uam.es)
+ */
+
+public class Contrato {
+	private Fecha fechaInicio;
+	private Fecha fechaFin;
+	private Profesor profesor;
+	private double sueldoBase;
+
+	/**
+	 * Contructor de la clase Contrato
+	 * Crea un nuevo objeto de tipo contrato especificando la fecha de inicio
+	 * y la de vencimiento.
+	 * 
+	 * @param dia1 Dia de inicio del contrato
+	 * @param mes1 Mes de inicio del contrato
+	 * @param anyo1 anyo de inicio del contrato
+	 * @param dia2 Dia de fin del contrato
+	 * @param mes2 Mes de fin del contrato
+	 * @param anyo2 anyo de fin del contrato
+	 * @param profesor Profesor contratado por la autoescuela
+	 * @param sueldoBase Cantidad fija que gana el profesor al mes
+	 */
+	public Contrato(int dia1, int mes1, int anyo1, int dia2, int mes2, int anyo2, 
+					Profesor profesor, double sueldoBase){
+		this.fechaInicio = new Fecha(dia1, mes1, anyo1);
+		this.fechaInicio = new Fecha(dia2, mes2, anyo2);
+		this.profesor = profesor;
+		this.sueldoBase = sueldoBase;
+	}
+
+	/**
+	 * Contructor de la clase Contrato
+	 * Crea un nuevo objeto de tipo contrato especificando la fecha de inicio.
+	 * Por defecto configura la fecha de vencimiento como 0/0/0
+	 * 
+	 * @param dia1 Dia de inicio del contrato
+	 * @param mes1 Mes de inicio del contrato
+	 * @param anyo1 anyo de inicio del contrato
+	 * @param profesor Profesor contratado por la autoescuela
+	 * @param sueldoBase Cantidad fija que gana el profesor al mes
+	 */
+	
+	public Contrato(int dia1, int mes1, int anyo1, Profesor profesor, double sueldoBase) {
+		this.fechaInicio = new Fecha(dia1, mes1, anyo1);
+		this.fechaFin = new Fecha(0,0,0);
+		this.profesor = profesor;
+		this.sueldoBase = sueldoBase;
+	}
+	
+	/**
+	 * Agrega la fecha de vencimiento de un contrato
+	 * 
+	 * @param dia1 Dia de vencimiento del contrato
+	 * @param mes1 Mes de vencimeinto
+	 * @param anyo1 anyo de vencimiento
+	 */
+	
+	public void setFin(int dia1, int mes1, int anyo1){
+		this.fechaFin = new Fecha(dia1, mes1, anyo1);
+	}
+	
+	/**
+	 * Metodo que devuelve la fecha de inicio del contrato
+	 * 
+	 * @return Fecha de inicio del contrato (Fecha)
+	 */
+	
+	public Fecha getIni(){
+		return this.fechaInicio;
+	}
+	
+	/**
+	 * Devuelve la fecha de vencimiento de un contrato
+	 * 
+	 * @return Fecha de vencimiento del contrato (Fecha)
+	 */
+	
+	public Fecha getFin(){
+		return this.fechaFin;
+	}
+	
+	/**
+	 * Devulve la cantidad fija de dinero que el profesor 
+	 * gana al mes
+	 * 
+	 * @return La cantidad de dinero (double)
+	 */
+	
+	public double getSueldoBase(){
+		return this.sueldoBase;
+	}
+	
+	/**
+	 * Devuelve el profesor que toma parte en el contrato
+	 * 
+	 * @return Un objeto de tipo Profesor
+	 */
+	
+	public Profesor getProfesor(){
+		return this.profesor;
+	}
+	
+	
+}
---
+++ Practica2/GR2201_LuisDavid/Apartado4/src/eps/uam/ads/p2/autoescuela/ejercicio4/MainApartado4.java
@@ -0,0 +1,48 @@
+package eps.uam.ads.p2.autoescuela.ejercicio4;
+
+/**
+ * Esta aplicacion prueba las clases Autoescuela y Profesor
+ * 
+ * @author David Garcia (david.garcia03@estudiante.uam.es)
+ * @author Luis Carabe (luis.carabe@estudiante.uam.es)
+ */
+
+public class MainApartado4 {
+	
+	/**
+	 * Punto de entrada de la aplicacion.
+	 * 
+	 * Este metodo realiza las acciones de crear una sede de una autoescuela,
+	 * dos profesores, realiza un contrato y, tras finalizar el contrato imprime
+	 * por pantalla la informacion de los contratos (periodo de tiempo que estuvo
+	 * vigente)
+	 * 
+	 * @param args No se esperan argumentos de entrada
+	 * */
+	
+	public static void main(String[] args){
+		
+		/*Creamos unas sedes de autoescuela*/
+		Autoescuela a1 = new Autoescuela(1, "Arenal", "c/ Del Río 4", "Pedro Pérez", 5, 100);
+		Autoescuela a2 = new Autoescuela(2, "Arenal 2", "c/ La Alcarria 8", "Inés Fernández", 6, 100);
+		
+		/*Creamos dos profesores*/
+		Profesor p1 = new Profesor(03145412, "Francisco", "Lozano", 1234455, 124526);
+		Profesor p2 = new Profesor(03143222, "María", "García", 1345667, 876567);
+		
+		/*Son contratados*/
+		a1.addContrato(p1, 2, 4, 2017, 500);
+		a1.addContrato(p2, 3, 5, 2016, 600);
+		
+		/*Son trasladados*/
+		a1.trasladarProfesor(03145412, 31, 12, 2018, a2);
+		
+		/*Son despedidos*/
+		a1.despedir(03143222, 21, 10, 2017);
+		
+		/*Obtenemos los datos para las autoescuelas*/
+		a1.imprimirPeriodos();
+		a2.imprimirPeriodos();
+		
+	}
+}
